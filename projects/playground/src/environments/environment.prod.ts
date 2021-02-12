1c56551bbf15a45a7f112e0c2941442e816eb76b
Thibaud RENAUX <thibaud.renaux@gmail.com>
2021-02-12 08:55:26 +0100

    missing file previous commit

--- projects/playground/src/environments/environment.prod.ts
+++ projects/playground/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
     audience: "https://cryptr-angular-demo.onrender.com",
     tenant_domain: "shark-academy",
     client_id: "04a51f2c-a7ea-4399-a5cb-9f7dbe3ece53",
-    default_redirect_uri: "https://cryptr-angular-demo.onrender.com/mes-formations",
+    default_redirect_uri: "https://cryptr-angular-demo.onrender.com/",
     httpInterceptor: {
       apiRequestsToSecure: [
         'https://cryptr-angular-express-backend.onrender.com/*',
