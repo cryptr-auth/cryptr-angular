cdf401b9b94bf60fa1f6912b43494038a09679d5
Thibaud RENAUX <thibaud.renaux@gmail.com>
2021-02-12 09:43:14 +0100

    use new angular playground app

--- projects/playground/src/environments/environment.prod.ts
+++ projects/playground/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   cryptrConfig: {
     audience: "https://cryptr-angular-playground.onrender.com",
     tenant_domain: "shark-academy",
-    client_id: "04a51f2c-a7ea-4399-a5cb-9f7dbe3ece53",
+    client_id: "d3397950-379e-4f65-8dd4-4ae9cca63401",
     cryptr_base_url: "https://cleeck-umbrella-staging-eu.onrender.com",
     default_redirect_uri: "https://cryptr-angular-playground.onrender.com/",
     default_locale: "fr",
