6d3d5701b6fe5db9a6a410f3b618415d1ba0501a
Thibaud RENAUX <thibaud.renaux@gmail.com>
2021-02-12 09:27:06 +0100

    improve prod env settings

--- projects/playground/src/environments/environment.prod.ts
+++ projects/playground/src/environments/environment.prod.ts
@@ -1,16 +1,18 @@
 export const environment = {
   production: true,
   cryptrConfig: {
-    audience: "https://cryptr-angular-demo.onrender.com",
+    audience: "https://cryptr-angular-playground.onrender.com",
     tenant_domain: "shark-academy",
     client_id: "04a51f2c-a7ea-4399-a5cb-9f7dbe3ece53",
-    default_redirect_uri: "https://cryptr-angular-demo.onrender.com/",
+    cryptr_base_url: "https://cleeck-umbrella-staging-eu.onrender.com",
+    default_redirect_uri: "https://cryptr-angular-playground.onrender.com/",
+    default_locale: "fr",
     httpInterceptor: {
       apiRequestsToSecure: [
-        'https://cryptr-angular-express-backend.onrender.com/*',
+        'https://cryptr-angular-express-staging-backend.onrender.com/*',
       ]
     },
     telemetry: false
   },
-  resource_server_url: 'https://cryptr-angular-express-backend.onrender.com',
+  resource_server_url: 'https://cryptr-angular-express-staging-backend.onrender.com',
 };
