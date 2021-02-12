408baaea9f0023aa320a6aa68568db6894c6b506
Thibaud RENAUX <thibaud.renaux@gmail.com>
2021-02-12 09:11:34 +0100

    clean up cryptr client factory

--- projects/cryptr/cryptr-angular/src/lib/auth.client.ts
+++ projects/cryptr/cryptr-angular/src/lib/auth.client.ts
@@ -2,32 +2,13 @@ import { InjectionToken } from '@angular/core';
 import CryptrSpa from '@cryptr/cryptr-spa-js';
 
 import { AuthClientConfig } from './auth.config';
-const DEFAULT_CONFIG = {
-  audience: 'http://localhost:4200',
-  tenant_domain: 'shark-academy',
-  client_id: '724b141a-e1eb-4f5b-bfca-22eca8ae3cc4',
-  default_redirect_uri: 'http://localhost:4200/',
-  // region: 'us',
-  cryptr_base_url: 'http://localhost:4000',
-  default_locale: 'fr',
-  httpInterceptor: {
-    apiRequestsToSecure: [
-      'http://localhost:5000/*',
-    ]
-  },
-  telemetry: false
-};
-
 export class CryptrClientFactory {
   static createClient(configFactory: AuthClientConfig): any {
     if (!!configFactory) {
       console.log('factory')
       console.debug(configFactory.get())
-    } else {
-      console.log('DEFAULT_CONFIG');
-      console.debug(DEFAULT_CONFIG);
     }
-    const config = configFactory !== null ? configFactory.get() : DEFAULT_CONFIG;
+    const config = configFactory.get();
 
     if (!config) {
       throw new Error(
