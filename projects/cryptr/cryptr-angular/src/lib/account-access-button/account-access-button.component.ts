7505694148c5a79eea0c6e8d35966de784d9f349
Thibaud RENAUX <thibaud.renaux@gmail.com>
2021-02-15 09:42:34 +0100

    follow codacy feedback

--- projects/cryptr/cryptr-angular/src/lib/account-access-button/account-access-button.component.ts
+++ projects/cryptr/cryptr-angular/src/lib/account-access-button/account-access-button.component.ts
@@ -56,7 +56,7 @@ export class AccountAccessButtonComponent implements OnChanges {
   errorBtnClass = ERROR_BTN_CLASS;
 
   closeAccountPopup(): void {
-    if (this.accountPopup !== undefined && this.accountPopup !== null) {
+    if (typeof this.accountPopup !== "undefined" && this.accountPopup !== null) {
       this.accountPopup.close();
     }
   }
