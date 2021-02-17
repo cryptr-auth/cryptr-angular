b600e02955d2ba472e4c5480de692945fa4f3c18
Thibaud RENAUX <thibaud.renaux@gmail.com>
2021-02-17 13:58:41 +0100

    updateCurrentAuthState in the right way

--- projects/cryptr/cryptr-angular/src/lib/auth.service.ts
+++ projects/cryptr/cryptr-angular/src/lib/auth.service.ts
@@ -215,15 +215,21 @@ export class AuthService implements OnDestroy {
       // } else if (this.cryptrClient.canHandleInvitation()) {
       //   console.log('can handle invite')
     } else {
-      this.cryptrClient.handleRefreshTokens().then((res) => {
-        console.log(`handlerefreshTokens ${res === true}`);
-        console.log(res);
-        this.updateCurrentAuthState(res === true);
-      }).catch((error) => {
+      await this.cryptrClient.handleRefreshTokens();
+      this.isAuthenticated().then((isAuthenticated) => {
+        this.updateCurrentAuthState(isAuthenticated);
+      }).catch((err) => {
         this.updateCurrentAuthState(false);
-      }).finally(() => {
-        this.isLoading$.next(false);
       });
+      // this.cryptrClient.handleRefreshTokens().then((res) => {
+      //   console.log(`handlerefreshTokens ${res === true}`);
+      //   console.log(res);
+      //   this.updateCurrentAuthState(res === true);
+      // }).catch((error) => {
+      //   this.updateCurrentAuthState(false);
+      // }).finally(() => {
+      //   this.isLoading$.next(false);
+      // });
     }
   }
 
