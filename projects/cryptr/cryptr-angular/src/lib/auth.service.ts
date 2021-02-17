4b835716182a580aa49122966e83cf660c3b6fa1
Thibaud RENAUX <thibaud.renaux@gmail.com>
2021-02-17 13:34:01 +0100

    update SPA dep

--- projects/cryptr/cryptr-angular/src/lib/auth.service.ts
+++ projects/cryptr/cryptr-angular/src/lib/auth.service.ts
@@ -149,6 +149,7 @@ export class AuthService implements OnDestroy {
 
 
   currentAuthenticationState(): boolean {
+    console.log(`currentAuthenticationState ${this.authenticated$.value}`)
     return this.authenticated$.value;
   }
 
