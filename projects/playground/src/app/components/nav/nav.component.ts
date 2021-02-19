9c0cf1d23528b8379a45732903ae9e21d7f2f20b
Thibaud RENAUX <thibaud.renaux@gmail.com>
2021-02-19 15:14:24 +0100

    use double quote

--- projects/playground/src/app/components/nav/nav.component.ts
+++ projects/playground/src/app/components/nav/nav.component.ts
@@ -19,7 +19,7 @@ export class NavComponent implements OnInit {
   //  O: normal, 1: invalid_grant, 2: expired
   logOutPopupStyle = 0;
   locale = 'fr';
-  signType = 'signin';
+  signType = "signin";
   redirectUri = 'http://localhost:4200/';
 
   constructor(public auth: AuthService, private router: Router) {
