340cbaa042a98f41fc426cdae512ab14f29f58d9
Thibaud RENAUX <thibaud.renaux@gmail.com>
2021-02-19 14:03:40 +0100

    test client in app module playground

--- projects/playground/src/app/app.module.ts
+++ projects/playground/src/app/app.module.ts
@@ -11,13 +11,16 @@ import { GraphqlComponent } from './pages/graphql/graphql.component';
 import { GrowthHackerComponent } from './pages/growth-hacker/growth-hacker.component';
 import { MyCoursesComponent } from './pages/my-courses/my-courses.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-// import { CommonModule } from '@angular/common'
-// import * as Sentry from '@sentry/angular';
 import { Router } from '@angular/router';
 import { environment } from '../environments/environment';
 import { AuthModule } from 'projects/cryptr/cryptr-angular/src/lib/auth.module';
 import { AccountAccessButtonComponent } from 'projects/cryptr/cryptr-angular/src/lib/account-access-button/account-access-button.component';
 import { AuthHttpInterceptor } from 'projects/cryptr/cryptr-angular/src/lib/auth.interceptor';
+import CryptrSpa from 'projects/cryptr/cryptr-angular/node_modules/@cryptr/cryptr-spa-js/dist/types/main';
+
+console.log('appmodule before client')
+const client = new CryptrSpa.client(environment.cryptrConfig);
+console.log('appmodule after client')
 @NgModule({
   declarations: [
     AppComponent,
