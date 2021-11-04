964d83d4a0d549577ac89b3289191d469a13b5d4
Thibaud RENAUX <thibaud.renaux@gmail.com>
2021-11-04 11:01:33 +0100

    move undefined test

--- projects/cryptr/cryptr-angular/src/lib/auth.service.ts
+++ projects/cryptr/cryptr-angular/src/lib/auth.service.ts
@@ -118,8 +118,9 @@ export class AuthService implements OnDestroy {
    * @returns process logout of session with callback call
    */
   logOut(callback: () => void, location: undefined | globalThis.Location = window.location, targetUrl?: string): Observable<any> {
-    alert(targetUrl)
-    return from(this.cryptrClient.logOut(this.preLogOutCallBack(callback), location, targetUrl));
+    let target = targetUrl === undefined ? window.location.href : targetUrl
+    alert(target)
+    return from(this.cryptrClient.logOut(this.preLogOutCallBack(callback), location, target));
   }
 
   /** @ignore */
