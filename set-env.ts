103b0a79c4108018d4ae7bde10703cecf9625fb1
Thibaud RENAUX <thibaud.renaux@gmail.com>
2022-08-03 19:03:18 +0200

    rollback set-env to CRYPTR_CONFIG env

--- set-env.ts
+++ set-env.ts
@@ -1,26 +1,10 @@
 const fs = require('fs');
 require('dotenv').config();
 
-if (process.env.PRODUCTION !== undefined && process.env.CRYPTR_CLIENT_ID !== undefined && process.env.RS_URL !== undefined) {
-  var cryptrConfig = {
-    audience: process.env.CRYPTR_AUDIENCE,
-    tenant_domain: process.env.CRYPTR_TENANT_DOMAIN,
-    client_id: process.env.CRYPTR_CLIENT_ID,
-    cryptr_base_url: process.env.CRYPTR_BASE_URL,
-    default_redirect_uri: process.env.CRYPTR_REDIRECT_URI,
-    default_locale: process.env.CRYPTR_LOCALE || 'en',
-    httpInterceptor: {
-      apiRequestsToSecure: [
-        process.env.RS_URL,
-      ]
-    },
-    telemetry: process.env.CRYPTR_TELEMETRY == 'true',
-    dedicated_server: process.env.CRYPTR_DEDICATED_SERVER == 'true',
-  }
-  console.debug(cryptrConfig);
+if (process.env.PRODUCTION !== undefined && process.env.CRYPTR_CONFIG !== undefined && process.env.RS_URL !== undefined) {
   const envConfigFile = `export const environment = {
     production: ${process.env.PRODUCTION},
-    cryptrConfig: ${cryptrConfig},
+    cryptrConfig: ${process.env.CRYPTR_CONFIG},
     resource_server_url: '${process.env.RS_URL}',
     idpIds: [${process.env.CRYPTR_IDP_IDS.split(',')}],
     targetUrl: '${process.env.CRYPTR_TARGET_URL}'
