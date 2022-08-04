805f695955aabbb56a85b9e1903119f3587373bc
Thibaud RENAUX <thibaud.renaux@gmail.com>
2022-08-04 09:21:25 +0200

    rollback env prod to previous state

--- projects/playground/src/environments/environment.prod.ts
+++ projects/playground/src/environments/environment.prod.ts
@@ -1,21 +1,20 @@
 export const environment = {
   production: true,
   cryptrConfig: {
-    audience: "https://cryptr-angular-playground-pr-47.onrender.com",
-    tenant_domain: "areon-holdings-co",
-    client_id: "fb6676d4-af7d-4703-805d-ffc693d31ea2",
+    audience: "https://cryptr-angular-playground.onrender.com",
+    tenant_domain: "comcast",
+    client_id: "30b3cfdd-2579-4252-b9a2-11c47a107d72",
     cryptr_base_url: "https://cleeck-umbrella-staging-eu.onrender.com",
-    default_redirect_uri: "https://cryptr-angular-playground-pr-47.onrender.com/",
+    default_redirect_uri: "https://cryptr-angular-playground.onrender.com/",
     default_locale: "fr",
     httpInterceptor: {
       apiRequestsToSecure: [
         'https://cryptr-express-staging-backend.onrender.com/*',
       ]
     },
-    telemetry: false,
-    dedicated_server: true,
+    telemetry: false
   },
   resource_server_url: 'https://cryptr-express-staging-backend.onrender.com',
-  idpIds: ["comcast_RrmZYfWTWncWmQ26QPVgbe", "flametech_B468fhGyJgYKauZSXbdxVg", "envirogreen_agency_LNAFPoFHsKzGVVujYKjQQR", "metro_cash_RbYDv2qHSyVJ96h6VkthjV"],
-  targetUrl: "https://cryptr-angular-playground-pr-47.onrender.com",
+  idpId: "comcast_RrmZYfWTWncWmQ26QPVgbe",
+  targetUrl: "https://cryptr-angular-playground.onrender.com",
 };
