e358b8e8e337a7d14b2689024e46aae87b4d063e
Thibaud RENAUX <thibaud.renaux@gmail.com>
2022-08-05 18:13:45 +0200

    Fix playground env

--- projects/playground/src/environments/environment.prod.ts
+++ projects/playground/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   cryptrConfig: {
     audience: "https://cryptr-angular-playground.onrender.com",
-    tenant_domain: "comcast",
+    tenant_domain: "areon-holdings-co",
     client_id: "30b3cfdd-2579-4252-b9a2-11c47a107d72",
     cryptr_base_url: "https://cleeck-umbrella-staging-eu.onrender.com",
     default_redirect_uri: "https://cryptr-angular-playground.onrender.com/",
@@ -12,9 +12,10 @@ export const environment = {
         'https://cryptr-express-staging-backend.onrender.com/*',
       ]
     },
-    telemetry: false
+    telemetry: false,
+    dedicated_server: true,
   },
   resource_server_url: 'https://cryptr-express-staging-backend.onrender.com',
-  idpIds: ["comcast_RrmZYfWTWncWmQ26QPVgbe"],
+  idpIds: ["comcast_RrmZYfWTWncWmQ26QPVgbe", "envirogreen_agency_LNAFPoFHsKzGVVujYKjQQR"],
   targetUrl: "https://cryptr-angular-playground.onrender.com",
 };
