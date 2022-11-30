e08a3bf8640f939eddae1433652c1287c22ebc1c
Thibaud RENAUX <thibaud.renaux@gmail.com>
2022-11-30 16:28:45 +0100

    add env varibales in prod env

--- projects/playground/src/environments/environment.prod.ts
+++ projects/playground/src/environments/environment.prod.ts
@@ -1,22 +1,20 @@
 export const environment = {
   production: true,
   cryptrConfig: {
-    audience: "https://cryptr-angular-playground.onrender.com",
-    tenant_domain: "areon-holdings-co",
-    client_id: "30b3cfdd-2579-4252-b9a2-11c47a107d72",
-    cryptr_base_url: "https://cleeck-umbrella-staging-eu.onrender.com",
-    default_redirect_uri: "https://cryptr-angular-playground.onrender.com/",
-    default_locale: "fr",
+    audience: process.env.CRYPTR_AUDIENCE || "https://cryptr-angular-playground.onrender.com",
+    tenant_domain: process.env.CRYPTR_TENANT_DOMAIN || "areon-holdings-co",
+    client_id: process.env.CRYPTR_CLIENT_ID || "30b3cfdd-2579-4252-b9a2-11c47a107d72",
+    cryptr_base_url: process.env.CRYPTR_BASE_URM || "https://cleeck-umbrella-staging-eu.onrender.com",
+    default_redirect_uri: process.env.CRYPTR_DEFAULT_REDIRECT_URI || "https://cryptr-angular-playground.onrender.com/",
+    default_locale: process.env.CRYPTR_DEFAULT_LOCALE || "fr",
     httpInterceptor: {
-      apiRequestsToSecure: [
-        'https://cryptr-express-staging-backend.onrender.com/*',
-      ]
+      apiRequestsToSecure: process.env.CRYPTR_API_TO_SECURE ? process.env.CRYPTR_API_TO_SECURE.split(',') : ['https://cryptr-express-staging-backend.onrender.com/*']
     },
     telemetry: false,
     dedicated_server: true,
     preferedAuthMethod: 'gateway'
   },
-  resource_server_url: 'https://cryptr-express-staging-backend.onrender.com',
-  idpIds: ["comcast_RrmZYfWTWncWmQ26QPVgbe", "envirogreen_agency_LNAFPoFHsKzGVVujYKjQQR"],
-  targetUrl: "https://cryptr-angular-playground.onrender.com",
+  resource_server_url: process.env.CRYPTR_RESOURCE_SERVER_URL || 'https://cryptr-express-staging-backend.onrender.com',
+  idpIds: process.env.CRYPTR_IDP_IDS ? process.env.CRYPTR_IDP_IDS.split(',') : ["comcast_RrmZYfWTWncWmQ26QPVgbe", "envirogreen_agency_LNAFPoFHsKzGVVujYKjQQR"],
+  targetUrl: process.env.CRYPTR_TARGET_URL || "https://cryptr-angular-playground.onrender.com",
 };
