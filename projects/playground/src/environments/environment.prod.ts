8aa651f1dc043aaa521f5ac441c3d4dce3c089c2
Thibaud RENAUX <thibaud.renaux@gmail.com>
2024-03-08 10:21:24 +0100

    fix environment.prod

--- projects/playground/src/environments/environment.prod.ts
+++ projects/playground/src/environments/environment.prod.ts
@@ -1,17 +1,17 @@
 export const environment = {
   production: true,
   cryptrConfig: {
-    audience: process.env.CRYPTR_AUDIENCE || "https://your-prod.angular.app",
-    tenant_domain: process.env.CRYPTR_TENANT_DOMAIN || "your-tenant-domai",
-    client_id: process.env.CRYPTR_CLIENT_ID || "your-client-id",
-    cryptr_base_url: process.env.CRYPTR_BASE_URL || "https://auth.cryptr.dev",
-    default_redirect_uri: process.env.CRYPTR_DEFAULT_REDIRECT_URI || "https://your-prod.angular.app",
+    audience: process.env.CRYPTR_AUDIENCE || 'https://your-prod.angular.app',
+    tenant_domain: process.env.CRYPTR_TENANT_DOMAIN || 'your-tenant-domai',
+    client_id: process.env.CRYPTR_CLIENT_ID || 'your-client-id',
+    cryptr_base_url: process.env.CRYPTR_BASE_URL || 'https://auth.cryptr.dev',
+    default_redirect_uri: process.env.CRYPTR_DEFAULT_REDIRECT_URI || 'https://your-prod.angular.app',
     default_slo_after_revoke: false,
     httpInterceptor: {
-      apiRequestsToSecure: process.env.CRYPTR_API_TO_SECURE || []
+      apiRequestsToSecure: process.env.CRYPTR_API_TO_SECURE ? process.env.CRYPTR_API_TO_SECURE.split(',') : ['https://your.backend.api']
     },
     dedicated_server: true,
   },
-  resource_server_url: process.env.CRYPTR_RESOURCE_SERVER_URL || "https://your.backend.api",
-  targetUrl: process.env.CRYPTR_TARGET_URL || "https://your-prod.angular.app",
+  resource_server_url: process.env.CRYPTR_RESOURCE_SERVER_URL || 'https://your.backend.api',
+  targetUrl: process.env.CRYPTR_TARGET_URL || 'https://your-prod.angular.app',
 };
