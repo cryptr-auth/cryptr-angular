9f5c65a917f14625b715be41817f28e2ea02ec6e
Thibaud RENAUX <thibaud.renaux@gmail.com>
2024-04-25 18:29:01 +0200

    codacy

--- projects/playground/src/environments/environment.prod.ts
+++ projects/playground/src/environments/environment.prod.ts
@@ -3,19 +3,20 @@ if (process === undefined) {
 }
 
 export const environment = {
-  production: true,
   cryptrConfig: {
     audience: (process && process.env.CRYPTR_AUDIENCE) || 'https://your-prod.angular.app',
-    tenant_domain: (process && process.env.CRYPTR_TENANT_DOMAIN) || 'your-tenant-domai',
     client_id: (process && process.env.CRYPTR_CLIENT_ID) || 'your-client-id',
     cryptr_base_url: (process && process.env.CRYPTR_BASE_URL) || 'https://auth.cryptr.dev',
+    dedicated_server: true,
     default_redirect_uri: (process && process.env.CRYPTR_DEFAULT_REDIRECT_URI) || 'https://your-prod.angular.app',
     default_slo_after_revoke: false,
     httpInterceptor: {
-      apiRequestsToSecure: (process && process.env.CRYPTR_API_TO_SECURE) ? process.env.CRYPTR_API_TO_SECURE.split(',') : ['https://your.backend.api']
+      apiRequestsToSecure: (process && process.env.CRYPTR_API_TO_SECURE) ?
+        process.env.CRYPTR_API_TO_SECURE.split(',') : ['https://your.backend.api'],
     },
-    dedicated_server: true,
+    tenant_domain: (process && process.env.CRYPTR_TENANT_DOMAIN) || 'your-tenant-domain',
   },
+  production: true,
   resource_server_url: (process && process.env.CRYPTR_RESOURCE_SERVER_URL) || 'https://your.backend.api',
   targetUrl: (process && process.env.CRYPTR_TARGET_URL) || 'https://your-prod.angular.app',
 };
